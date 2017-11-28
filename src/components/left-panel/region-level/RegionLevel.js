3daa37652616d30eaadd8e84fd91bfe60f2158e2
nghitrum <nghitrum@gmail.com>
2017-11-28 11:27:47 +0200

    resolve conflict

--- src/components/left-panel/region-level/RegionLevel.js
+++ src/components/left-panel/region-level/RegionLevel.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
-
-import Select from 'react-select';
-
-import 'react-select/dist/react-select.css';
+import React, { Component } from "react";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
+import ForestData from "../../../data/ForestData";
 
 class RegionLevels extends Component {
-    render () {
-        return (
-            <div>
-                <h4>Aluetaso</h4>
-                    <Select
-                        name=""
-                        className="max"
-                        //value={}
-                        //onChange={}
-                        options={[
-                            { value: 'one', label: 'One' },
-                            { value: 'two', label: 'Two' },
-                        ]}
-                    />
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+    let regionLevelList = [];
+  }
+  componentDidMount() {
+    this.regionLevelList = [
+      {
+        id: 1,
+        name: "hello"
+      },
+      {
+        id: 2,
+        name: "world"
+      }
+		];
+		
+		// get data from ForestData.js
+  }
+  render() {
+    return (
+      <div>
+        <h4>Aluetaso</h4>
+        <Select
+          name=""
+          className="max"
+          //value={}
+          //onChange={}
+          options={[{ value: this.regionLevelList.id, label: this.regionLevelList.name }]}
+        />
+      </div>
+    );
+  }
 }
 
-export default RegionLevels
+export default RegionLevels;
--- src/components/left-panel/region/Region.js
+++ src/components/left-panel/region/Region.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
 
-import Select from 'react-select';
-
-import 'react-select/dist/react-select.css';
+import ForestData from "../../../data/ForestData";
 
 class Region extends Component {
-    render () {
-        return (
-            <div>
-                <h4>Alue</h4>
-                <Select
-                    name=""
-                    className=""
-                    //value={}
-                    //onChange={}
-                    options={[
-                        { value: 'one', label: 'One' },
-                        { value: 'two', label: 'Two' },
-                    ]}
-                />
-            </div>
-        )
-    }
+  
+
+  render() {
+    const { id, name } = this.props;
+    return (
+      <div>
+        <h4>Alue</h4>
+        <Select
+          name=""
+          className=""
+          //value={}
+          //onChange={}
+          options={[{ value: this.props.id, label: this.props.name }]}
+        />
+      </div>
+    );
+  }
 }
 
-export default Region
+export default Region;
