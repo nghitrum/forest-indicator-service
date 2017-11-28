f720d8039c274f08afe575675bc1aebf6df886ba
nghitrum <nghitrum@gmail.com>
2017-11-28 11:27:47 +0200

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.scss';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./App.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import Header from './components/header/header';
-import RightPanel from './components/right-panel/RightPanel';
-import LeftPanel from './components/left-panel/LeftPanel';
-import ChartContainer from './components/chart-container/ChartContainer';
+import Header from "./components/header/header";
+import RightPanel from "./components/right-panel/RightPanel";
+import LeftPanel from "./components/left-panel/LeftPanel";
+import ChartContainer from "./components/chart-container/ChartContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid App">
         <Header />
-        
+
         <div className="col-lg-2">
           <LeftPanel />
         </div>
@@ -25,7 +25,6 @@ class App extends Component {
         <div className="col-lg-2">
           <RightPanel />
         </div>
-
       </div>
     );
   }
