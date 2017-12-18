96bd2b69006243689c2f5dcb834be6ae5e268cc2
nghitrum <nguyenvietnghi@live.com>
2017-12-19 01:04:28 +0200

    added bar chart

--- src/components/chart-container/ChartContainer.js
+++ src/components/chart-container/ChartContainer.js
@@ -1,67 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Chart from './chart/Chart';
-import MultiplePolarCharts from './multiple-polar-charts/MultiplePolarCharts';
-import BarChart from './barchart/BarChart';
-import TableChart from './table-chart/TableChart';
-import ChartControls from './chart-controls/ChartControls';
+import Chart from "./chart/Chart";
+import MultiplePolarCharts from "./multiple-polar-charts/MultiplePolarCharts";
+import BarChart from "./barchart/BarChart";
+import TableChart from "./table-chart/TableChart";
+import ChartControls from "./chart-controls/ChartControls";
 
-import './chartcontainer.scss';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./chartcontainer.scss";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 class ChartContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.setScenarioId = this.setScenarioId.bind(this);
 
-    constructor(props){
-        super(props);
-        this.setScenarioId = this.setScenarioId.bind(this);
+    this.state = {
+      chartType: "singlepolar"
+    };
 
-        this.state = {
-            chartType: "singlepolar"
-          };
+    this.changeChartType = this.changeChartType.bind(this);
+    this.RenderChart = this.RenderChart.bind(this);
+  }
 
-          this.changeChartType = this.changeChartType.bind(this);
-          this.RenderChart = this.RenderChart.bind(this);
-    }
+  setScenarioId() {
+    console.log(this.props.options);
+  }
 
-    setScenarioId(){
-        console.log(this.props.options)
-    }
+  changeChartType(type) {
+    this.setState({
+      chartType: type
+    });
+  }
 
-    changeChartType(type) {
-        this.setState({
-          chartType: type
-        });
-    }
-
-    RenderChart(chartType){
-        switch(chartType){
-            case "singlepolar":
-                return <Chart />
-            case "manypolar":
-                return <MultiplePolarCharts />
-            case "bar":
-                return <BarChart />
-            case "table":
-                return <TableChart />
-            default:
-                return <Chart />
-        }
-    }
-    
-    render () {
-
+  RenderChart(chartType) {
+    switch (chartType) {
+      case "singlepolar":
+        return <Chart />;
+      case "manypolar":
+        return <MultiplePolarCharts />;
+      case "bar":
         return (
-            <div className="chart-container">
-                <div className="chart-content">
-                    {this.RenderChart(this.state.chartType)}
-                </div>
-                <div className="chart-controls container">
-                    <ChartControls
-                        changeChartType={this.changeChartType} />
-                </div>
-            </div>
-        )
+          <BarChart
+            values={this.props.valueData}
+            options={this.props.options}
+            scenarios={this.props.scenarios}
+            regionalLevel={this.props.regionalLevel}
+            region={this.props.region}
+          />
+        );
+      case "table":
+        return <TableChart />;
+      default:
+        return <Chart />;
     }
+  }
+
+  render() {
+    return (
+      <div className="chart-container">
+        <div className="chart-content">
+          {this.RenderChart(this.state.chartType)}
+        </div>
+        <div className="chart-controls container">
+          <ChartControls changeChartType={this.changeChartType} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ChartContainer
+export default ChartContainer;
